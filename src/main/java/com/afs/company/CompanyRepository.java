cc7173552755bd3d61326713ae1caa1741649085
leungpo <polly.leung@oocl.com>
2022-12-06 22:41:50 +0800

    feat: add Companies in CompanyRepository

--- src/main/java/com/afs/company/CompanyRepository.java
+++ src/main/java/com/afs/company/CompanyRepository.java
@@ -1,4 +1,17 @@
 package com.afs.company;
 
+import com.afs.employee.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompanyRepository {
+    List<Company> companies;
+
+    public CompanyRepository(){
+        this.companies = new ArrayList<>();
+        companies.add(new Company(1, "abc Company"));
+        companies.add(new Company(2, "def Company"));
+        companies.add(new Company(3, "ghi Company"));
+    }
 }
